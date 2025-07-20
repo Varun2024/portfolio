998dff5e9e0c4d9f2e1d115f85d5ccbaf331ffd2
Varun2024 <marvellous079@gmail.com>
2025-07-20 22:19:15 +0530

    some more

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,24 +1,37 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
+import ProjectDetails from './ProjectDetails'
 
-const Project = ({title, description,suDescription,href,image, tags}) => {
+const Project = ({ title, description, subDescription, href, image, tags }) => {
+    const [isHidden, setIsHidden] = useState(false)
     return (
         <>
-        <div className=' flex-wrap items-start justify-between py-10 space-y-14 sm:flex sm:space-y-0'>
-            <div className="">
-                <p className="text-2xl">{title}</p>
-                <div className="flex gap-5 mt-2 text-[var(--color-sand)] ">
-                    {tags.map((tag)=>(
-                        <span key={tag.id}>{tag.name}</span>
-                    ))}
+            <div className=' flex-wrap items-start justify-between py-10 space-y-14 sm:flex sm:space-y-0'>
+                <div className="">
+                    <p className="text-2xl">{title}</p>
+                    <div className="flex gap-5 mt-2 text-[var(--color-sand)] ">
+                        {tags.map((tag) => (
+                            <span key={tag.id}>{tag.name}</span>
+                        ))}
+                    </div>
                 </div>
+                <button onClick={() => setIsHidden(true)} className='flex items-center gap-1 cursor-pointer hover-animation'>
+                    Read More
+                    <img src="assets/arrow-right.svg" />
+                </button>
             </div>
-            <button className='flex items-center gap-1 cursor-pointer hover-animation'>
-                Read More
-                <img src="assets/arrow-right.svg" />
-            </button>
-        </div>
-        <div className='bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full'/>
+            <div className='bg-gradient-to-r from-transparent via-neutral-700 to-transparent h-[1px] w-full' />
+            {isHidden &&
+                < ProjectDetails
+                    title={title}
+                    description={description}
+                    subDescription={subDescription}
+                    image={image}
+                    tags={tags}
+                    href={href}
+                    closeModal={() => setIsHidden(false)}
+                />
+            }
         </>
     )
 }
