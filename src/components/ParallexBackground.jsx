ae0a623bd238eec4b9dd52580159c035967c1f2f
Varun2024 <marvellous079@gmail.com>
2025-08-02 00:47:22 +0530

    hero subtle animation added

--- src/components/ParallexBackground.jsx
+++ src/components/ParallexBackground.jsx
@@ -1,50 +1,69 @@
 /* eslint-disable no-unused-vars */
 
-import {motion, useScroll, useSpring, useTransform } from "motion/react"
+import { animate } from "motion"
+import { motion, useMotionTemplate, useMotionValue, useScroll, useSpring, useTransform } from "motion/react"
+import { useEffect } from "react"
 
 
 const ParallexBackground = () => {
     const { scrollYProgress } = useScroll()
-    const x = useSpring(scrollYProgress,{damping:50})
+    const x = useSpring(scrollYProgress, { damping: 50 })
     // usetranform can transform on set of value into other of a property;mostly used for x nd y values
     const mountain3Y = useTransform(x, [0, 0.5], ["0%", "70%"])
     const planetY = useTransform(x, [0, 0.5], ["0%", "-20%"])
     const mountain2Y = useTransform(x, [0, 0.5], ["0%", "30%"])
     const mountainY = useTransform(x, [0, 0.5], ["0%", "0%"])
 
+    // for the changing background color
+    const COLORS_TOP = ["#13FFAA80", "#1E67C680", "#CE84CF80", "#DD335C80"];
+    const color = useMotionValue(COLORS_TOP[0]);
+
+    useEffect(() => {
+        animate(color, COLORS_TOP, {
+            ease: "easeInOut",
+            duration: 15,
+            repeat: Infinity,
+            repeatType: "mirror",
+        });
+    }, []);
+
+    const backgroundImage = useMotionTemplate`radial-gradient(300% 130% at 0% 100%, #020617 50%, ${color})`;
+
     return (
-        <section className='absolute inset-0 bg-black/40'>
-            <div className="relative h-screen overflow-y-hidden">
+        <motion.section className='absolute inset-0 '
+            style={{ backgroundImage ,zIndex:"1"}} 
+        >
+            <div className="relative h-screen overflow-y-hidden bg-black/30">
                 <motion.div className="absolute inset-0 w-full h-screen -z-40"
                     style={{
                         backgroundImage: "url(/assets/mountain-3.png)",
                         backgroundPosition: "bottom",
-                        backgroundSize: "cover", 
-                        y:mountain3Y,
+                        backgroundSize: "cover",
+                        y: mountain3Y,
                     }} />
                 <motion.div className="absolute inset-0 w-full h-screen -z-30"
                     style={{
                         backgroundImage: "url(/assets/planets.png)",
                         backgroundPosition: "bottom",
                         backgroundSize: "cover",
-                        x:planetY,
+                        x: planetY,
                     }} />
                 <motion.div className="absolute inset-0 w-full h-screen -z-20"
                     style={{
                         backgroundImage: "url(/assets/mountain-2.png)",
                         backgroundPosition: "bottom",
                         backgroundSize: "cover",
-                        y:mountain2Y
+                        y: mountain2Y
                     }} />
                 <motion.div className="absolute inset-0 w-full h-screen -z-10"
                     style={{
                         backgroundImage: "url(/assets/mountain-1.png)",
                         backgroundPosition: "bottom",
                         backgroundSize: "cover",
-                        y:mountainY
+                        y: mountainY
                     }} />
             </div>
-        </section>
+        </motion.section>
     )
 }
 
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -4,28 +4,38 @@ import { Float, OrbitControls } from "@react-three/drei"
 import HeroText from "../components/HeroText"
 import ParallexBackground from "../components/ParallexBackground"
 import { Canvas, useFrame } from "@react-three/fiber"
-import { Suspense, useState } from "react"
+import { Suspense, useEffect, useState } from "react"
 import Astronaut from "../components/Astronaut"
 import { useMediaQuery } from "react-responsive"
 import Loader from "../components/Loader"
+import { motion,useMotionTemplate, useMotionValue } from "motion/react"
+import { animate } from "motion"
+
+
 
 
 const Hero = () => {
+
+
     const isMobile = useMediaQuery({ maxWidth: 768 })
     return (
-        <section id="home" className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space">
+        <section
+            id="home"
+            className="flex items-start justify-center md:items-start md:justify-start min-h-screen overflow-hidden c-space"
+            
+        >
             <HeroText />
             <ParallexBackground />
             <figure className="absolute inset-0 z-10 "
                 style={{ width: "100vw", height: "100vh" }}>
                 <Canvas >
                     <Suspense fallback={<Loader />}>
-                    <Astronaut isMobile={isMobile} scale={isMobile && .03} position={isMobile && [.115, -5, -9.454]} />
-                    {/* needed light to show model otherwise it would be just a black outline there */}
-                    <ambientLight intensity={0.8} />
-                    <directionalLight position={[10, 10, 10]} intensity={2} />
-                    {/* <OrbitControls /> */}
-                    {/* to make the canvas dragable */}
+                        <Astronaut isMobile={isMobile} scale={isMobile && .03} position={isMobile && [.115, -5, -9.454]} />
+                        {/* needed light to show model otherwise it would be just a black outline there */}
+                        <ambientLight intensity={0.8} />
+                        <directionalLight position={[10, 10, 10]} intensity={2} />
+                        {/* <OrbitControls /> */}
+                        {/* to make the canvas dragable */}
                     </Suspense>
                 </Canvas>
             </figure>
